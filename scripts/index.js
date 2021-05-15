77e8c37c1a413a49728a0a0162a559e3c030f13c
Soham Chatterjee <sohamchatterjee2324@gmail.com>
2021-05-15 09:36:48 +0530

    Section B end

--- scripts/index.js
+++ scripts/index.js
@@ -28,4 +28,16 @@ $(document).ready(function(){
         }
     });
 
+    //Isotope filter
+    var $grid = $(".grid").isotope({
+        itemSelector: ".grid-item",
+        layoutMode: "fitRows"
+    });
+
+    //Filter items on button click
+    $(".button-group").on("click", "button", function(){
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({filter: filterValue});
+    });
+
 });
